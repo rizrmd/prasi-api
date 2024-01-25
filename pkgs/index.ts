176ee4f3099cb301c06ac811714743feb5e41d9d
Rizky <rizky05@gmail.com>
2024-01-25 12:21:48 +0700

    wip fix

--- pkgs/index.ts
+++ pkgs/index.ts
@@ -22,11 +22,11 @@ if (!(await existsAsync(dir("app")))) {
 }
 
 if (!(await existsAsync(dir("app/db")))) {
-  await $`unzip -o pkgs/zip/dbzip -d app/db`;
+  await $`unzip -o pkgs/zip/dbzip -d app`;
 }
 
 if (!(await existsAsync(dir("app/srv")))) {
-  await $`unzip -o pkgs/zip/srvzip -d app/srv`;
+  await $`unzip -o pkgs/zip/srvzip -d app`;
 }
 
 if (!process.env.PORT) {
