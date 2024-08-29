0825b55d820a2c8aec301c4ad3139e941302a26b
Rizky <rizky@avolut.com>
2024-08-29 10:10:49 +0700

    fix

--- pkgs/api/_dbs.ts
+++ pkgs/api/_dbs.ts
@@ -1,20 +1,33 @@
+import { gunzipSync } from "bun";
+import { unpack } from "msgpackr";
 import { apiContext } from "service-srv";
 import { execQuery } from "utils/query";
+
 const g = global as any;
 export const _ = {
   url: "/_dbs/*",
+  raw: true,
   async api() {
     const { req, res } = apiContext(this);
     if (typeof g.db !== "undefined") {
-      const body = req.params;
+      if (req.params._ === "check") {
+        return { mode: "encrypted" };
+      }
 
       try {
-        const result = await execQuery(body, g.db);
-        return result;
-      } catch (e: any) {
-        console.log("_dbs error", body, e.message);
+        const body = unpack(gunzipSync(await req.arrayBuffer()));
+
+        try {
+          const result = await execQuery(body, g.db);
+          return result;
+        } catch (e: any) {
+          console.log("_dbs error", body, e.message);
+          res.sendStatus(500);
+          res.send(e.message);
+        }
+      } catch (e) {
         res.sendStatus(500);
-        res.send(e.message);
+        res.send('{status: "unauthorized"}');
       }
     }
   },
